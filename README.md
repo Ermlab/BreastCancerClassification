cf14ceb2f8862df770ba249dbac029a4ac5a64c3
simongeek <plotkaszymon@gmail.com>
2018-09-14 13:53:43 +0200

    edit main script & add README

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# BreastCancerClassification
-Breast Cancer Classification using scikit-learn and Keras
+## Breast Cancer Classification using scikit-learn and Keras
+
+## Getting started
+
+First of all you need to make sure you have installed Python 3.6. For that purpose we recommend it has all the necessary libraries except:
+* pandas==0.23.4
+* seaborn==0.8.1
+* Keras==2.2.2
+* matplotlib==2.0.2
+* numpy==1.14.0
+* graphviz==0.8.2
+* scikit_learn==0.19.2
+
+All libraries can be installed with the following commands:
+
+```
+pip install pandas
+pip install seaborn
+pip install Keras
+pip install matplotlib
+pip install graphviz
+pip install scikit-learn
+```
+
+or quickly:
+```
+pip install -r requirements.txt
+```
+
+## Data
+
+For the project, I used a breast cancer dataset from Wisconsin University.
+The dataset contains 569 samples and 30 features computed from digital images.
+Each sample identifies parameters of each patient.
+
+## Useful repositories
+
+## Contact & Blog post
+* Main Autor [Szymon Płotka](https://github.com/simongeek)
+* CEO of [Ermlab Software](https://ermlab.com) [Krzysztof Sopyła](https://github.com/ksopyla)
+* Check Our blog post [Breast cancer classification using scikit-learn and Keras]()
--- classification.py
+++ classification.py
@@ -1,3 +1,5 @@
+"""Script for classify Breast Cancer"""
+
 # Import necessary packages
 
 import pandas as pd
@@ -6,6 +8,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
@@ -20,13 +24,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
+from sklearn.metrics import roc_curve
 from sklearn.model_selection import cross_val_score
 from sklearn.manifold import TSNE
 from sklearn import tree
 import graphviz
 
 # Load data
-data = pd.read_csv('Data/data.csv', delimiter=',', header=0)
+data = pd.read_csv('Data/data.csv', delimiter=',')
 
 # Head method show first 5 rows of data
 print(data.head())
@@ -42,13 +47,13 @@ y = data['diagnosis'].map(d)
 X = data.drop(columns, axis=1)
 
 # Plot number of M - malignant and B - benign cancer
-"""
+
 ax = sns.countplot(y, label="Count", palette="muted")
 B, M = y.value_counts()
-plt.savefig('count.png')
+plt.savefig('Plots/count.png')
 print('Number of benign cancer: ', B)
 print('Number of malignant cancer: ', M)
-"""
+
 # Split dataset into training (80%) and test (20%) set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
@@ -58,33 +63,29 @@ X_test_N = (X_test-X_train.mean())/(X_test.max()-X_test.min())
 
 ####### PCA ######
 
-"""
+
 # PCA without std
 pca = PCA()
 pca.fit(X)
-plt.figure(1)
 print(pca.explained_variance_ratio_)
+plt.figure()
 plt.plot(pca.explained_variance_ratio_)
 plt.xlabel('number of components')
 plt.ylabel('% of variance')
 plt.title('PCA without Std')
-plt.savefig('pcavariancewithoutstd.png')
-"""
+plt.savefig('Plots/pcavariancewithoutstd.png')
+
 # PCA with std
 pca = PCA(n_components=6)
 X_std = StandardScaler().fit_transform(X)
 pca.fit(X_std)
-plt.figure()
 print(pca.explained_variance_ratio_)
+plt.figure()
 plt.plot(pca.explained_variance_ratio_)
 plt.xlabel('number of components')
 plt.ylabel('% of variance')
 plt.title('PCA with Std')
-plt.savefig('pcavariancewithstd.png')
-
-
-# t-SNE for visualization
-
+#plt.savefig('Plots/pcavariancewithstd.png')
 
 ###### SVM ######
 
@@ -105,6 +106,10 @@ print("SVM accuracy: %0.2f (+/- %0.2f)" % (svm_score.mean(), svm_score.std() * 2
 ###### K-Nearest Neighbors ######
 
 def KnearestNeighbors():
+    """
+    Function for compute accuracy using K-NN algorithm
+    :return: k-NN score
+    """
     for i in range(1, 5):
         knn = KNeighborsClassifier(n_neighbors=i)
         knnp = Pipeline([
@@ -133,7 +138,13 @@ print("Decision Tree accuracy: %0.2f (+/- %0.2f)" % (score_trees.mean(), score_t
 feature_names = X.columns.values
 
 def plot_decision_tree1(a,b):
-    dot_data = tree.export_graphviz(a, out_file='tree.dot',
+    """
+    Function for plot decision tree
+    :param a: decision tree classifier
+    :param b: feature names
+    :return: graph
+    """
+    dot_data = tree.export_graphviz(a, out_file='Plots/tree.dot',
                              feature_names=b,
                              class_names=['Malignant','Benign'],
                              filled=False, rounded=True,
@@ -144,7 +155,6 @@ plot_decision_tree1(treeclf,feature_names)
 
 
 ###### Random Forests ######
-from sklearn.ensemble import RandomForestClassifier
 
 rf = RandomForestClassifier()
 rfp = Pipeline([
@@ -153,6 +163,17 @@ rfp = Pipeline([
 ])
 score_rf = cross_val_score(rfp, X, y, cv=10)
 print("Random Forest accuracy: %0.2f (+/- %0.2f)" % (score_rf.mean(), score_rf.std() * 2))
+
+###### Naive Bayes Classifier ######
+
+gnb = GaussianNB()
+gnbclf = gnb.fit(X_train_N, y_train)
+gnbp = Pipeline([
+    ('reduce_dim', pca),
+    ('gnb', gnb)
+])
+gnb_score = cross_val_score(gnb, X, y, cv=10, scoring='accuracy')
+print("Gaussian Naive Bayes accuracy: %0.2f (+/- %0.2f)" % (gnb_score.mean(), gnb_score.std() * 2))
 
 ###### Neural Networks ######
 
@@ -210,18 +231,6 @@ def l5neuralNetwork():
 
 l5neuralNetwork()
 
-###### Naive Bayes Classifier ######
-from sklearn.naive_bayes import GaussianNB
-
-gnb = GaussianNB()
-gnbclf = gnb.fit(X_train_N, y_train)
-gnbp = Pipeline([
-    ('reduce_dim', pca),
-    ('gnb', gnb)
-])
-gnb_score = cross_val_score(gnb, X, y, cv=10, scoring='accuracy')
-print("Gaussian Naive Bayes accuracy: %0.2f (+/- %0.2f)" % (gnb_score.mean(), gnb_score.std() * 2))
-
 
 ###### Classification Mark ######
 
@@ -234,7 +243,7 @@ df_cm = pd.DataFrame(cm, range(2),
 plt.figure(figsize=(10,7))
 sns.set(font_scale=1.4)#for label size
 cm_plot = sns.heatmap(df_cm, annot=True, fmt='n', annot_kws={"size": 12})# font size
-cm_plot.figure.savefig('confusionmatrix.png')
+cm_plot.figure.savefig('Plots/confusionmatrix.png')
 #plt.show()
 
 # Precision & Recall Score
@@ -244,7 +253,6 @@ print("Recall score {}%".format(round(recall_score(y_test, y_pred),3)))
 print("F1 Score {}%".format(round(f1_score(y_test, y_pred, average='weighted'),3)))
 
 # ROC Curve
-from sklearn.metrics import roc_curve
 y_score = model.fit(X_train_N, y_train).decision_function(X_test_N)
 
 fpr, tpr, thresholds = roc_curve(y_test, y_score)
@@ -257,11 +265,12 @@ plt.xlabel('False Positive Rate (1 - specificity)')
 plt.ylabel('True Positive Rate (sensitivity)')
 plt.title('ROC Curve for Breast Cancer Classifer')
 plt.legend(loc="lower right")
-plt.savefig('roccurve.png')
+plt.savefig('Plots/roccurve.png')
 
 # Correlation Map
 
+plt.figure()
 f, ax = plt.subplots(figsize=(14,14))
 corr_plot = sns.heatmap(X.corr(), annot=False, linewidths=.5, fmt='.1f', ax=ax)
-corr_plot.figure.savefig('corrmap.png')
+corr_plot.figure.savefig('Plots/corrmap.png')
 #plt.show()
